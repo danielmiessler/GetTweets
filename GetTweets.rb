14d93c6d43f46d5b884abb7c5f37fbb856f92e27
Daniel Miessler <daniel@danielmiessler.com>
2015-01-10 23:43:59 -0800

    Added GetTweets file

---
+++ GetTweets.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "YOURS"
+    config.consumer_secret     = "YOURS"
+    config.access_token        = "YOURS"
+    config.access_token_secret = "YOURS"
+end
+
+# Open tweets file
+filename = "/etc/scripts/5tweets.txt"
+filehandle = File.open(filename, "w")
+
+# Pull latest five tweets and display them
+client.user_timeline.take(5).collect do |tweet|
+     filehandle.puts  "<li>#{tweet.text}</li>"
+     filehandle.puts  "-"
+      end
+
+# Finish tweets file
+filehandle.puts '<p>:: <a href="https://twitter.com/intent/user?screen_name=DanielMiessler">twitter.com/yourusername</a></p>'
+filehandle.close
